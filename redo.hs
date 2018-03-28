df50826f2d23217412127185f59efac9803c7228
anderjackie <anderjackie7@gmail.com>
2018-03-27 21:33:34 -0300

    adding redo-ifchange script to rebuild the code only if a change is made and adapting redo.do to use it. Also modifing th redo.hs code to trace desired commands and exporting the environment for the scripts to use.

--- redo.hs
+++ redo.hs
@@ -1,11 +1,15 @@
 import Control.Monad (filterM, liftM)
+import Data.Map.Lazy (toList, fromList, insert, adjust)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (traceShow)
 import System.Directory (renameFile, removeFile, doesFileExist)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
-import System.Process (createProcess, waitForProcess, shell)
+import System.Process (createProcess, waitForProcess, shell, CreateProcess(..))
+
+traceShow' arg = traceShow arg arg
 
 main :: IO ()
 main =  mapM_ redo =<< getArgs
@@ -14,15 +18,17 @@ redo :: String -> IO ()
 redo target = maybe printMissing redo' =<< redoPath target
     where redo' :: FilePath -> IO ()
           redo' path = do
-            (_, _, _, ph) <- createProcess $ shell $ cmd path
+            oldEnv <- getEnvironment
+            let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
+            (_, _, _, ph) <- createProcess $ traceShow' $ (shell $ cmd path) {env = Just ([("REDO_TARGET", target)] ++ oldEnv)}
             exit <- waitForProcess ph
-            case exit of
+            case traceShow' exit of
                 ExitSuccess -> do renameFile tmp target
                 ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero code: " ++ show code
                                        removeFile tmp
           printMissing = (error $ "No .do file found for target '" ++ target ++ "'") 
           tmp = target ++ "---redoing"
-          cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
+          cmd path = traceShow' $ unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = listToMaybe `liftM` filterM doesFileExist candidates 
